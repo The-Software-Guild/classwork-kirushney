629862a5abd65bbf8215db2e778410c0f9024a3f
Kirushney Kalamohan <kirushney@hotmail.co.uk>
2022-02-15 17:54:01 +0000

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/kk/helloworld/Hello.java
@@ -0,0 +1,13 @@
+/**
+ *@author Kirushney Kalamohan
+ *email: kirushney@hotmail.co.uk
+ *date: 15/02/22
+ *purpose: My first Hello World program in an IDE
+ */
+package com.kk.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Kirushney!");
+    }
+}
