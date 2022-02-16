9783491c02588f001362c12da6ccea9820e2f942
Kirushney Kalamohan <kirushney@hotmail.co.uk>
2022-02-16 13:12:36 +0000

    Updated Exercises

---
+++ Classwork/PracticeProgramming/src/main/java/com/sg/foundations/scanner/MiniMadLibs.java
@@ -0,0 +1,11 @@
+/**
+ *@author Kirushney Kalamohan
+ *email: kirushney@hotmail.co.uk
+ *date: 16/02/22
+ *purpose: Programming Scanner Exercise 8
+ */
+package com.sg.foundations.scanner;
+
+public class MiniMadLibs {
+    
+}
---
+++ Classwork/PracticeProgramming/src/main/java/com/sg/foundations/variables/BestAdderEver.java
@@ -0,0 +1,22 @@
+/**
+ *@author Kirushney Kalamohan
+ *email: kirushney@hotmail.co.uk
+ *date: 16/02/22
+ *purpose: Programming Variables Exercise 6
+ */
+package com.sg.foundations.variables;
+
+public class BestAdderEver {
+    public static void main(String[] args) {
+        int number1, number2, number3, total;
+        
+        number1 = 1;
+        number2 = 2;
+        number3 = 3;
+        
+        total = number1 + number2 + number3;
+        
+        System.out.println(number1 + "+" + number2 + "+" + number3 + "" + "=" + total);
+          
+    }
+}
